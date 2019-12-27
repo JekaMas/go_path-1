3f902e5db2271d4202f7d04ec7cce857900f7f26
kmortyk <kmortyk@gmail.com>
2019-12-27 18:42:18 +0400

    Map shop 1/2.

---
+++ map/map_shop.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
+
+var prices = make(map[string]int) // prices[productName]price
+
+var savedOrders = make(map[string]int) // savedOrders[orderKey]price
+
+var users = make(map[string]int) // users[userName]amount
+
+/* --- Product ------------------------------------------------------------------------------------------------------ */
+
+// AddProduct add new product if not exists
+func AddProduct(productName string, price int) error {
+	_, ok := prices[productName]
+	if ok {
+		return errors.New("product already exists")
+	}
+
+	prices[productName] = price
+	return nil
+}
+
+// UpdatePrice update the price of an existing product
+func UpdatePrice(productName string, price int) error {
+	_, ok := prices[productName]
+	if !ok {
+		return errors.New("can't update nil product")
+	}
+
+	prices[productName] = price
+	return nil
+}
+
+/* --- Orders ------------------------------------------------------------------------------------------------------- */
+
+//
+func OrderPrice(order map[string]int) int { // map[name]count
+	price := 0
+
+	for name, count := range order {
+		price += prices[name] * count
+	}
+
+	return price
+}
+
+func OrderPriceCached(order map[string]int) int { // map[name]count
+	key := fmt.Sprint(order)
+
+	cached, ok := savedOrders[key]
+	if ok {
+		return cached
+	}
+
+	price := OrderPrice(order)
+	savedOrders[key] = price
+
+	return price
+}
+
+/* --- User --------------------------------------------------------------------------------------------------------- */
+
+func AddUser(userName string, amount int) error {
+	_, ok := prices[userName]
+	if ok {
+		return errors.New("user already exists")
+	}
+
+	users[userName] = amount
+	return nil
+}
+
+// MakeOrder
+func MakeOrder(userName string, order map[string]int) error {
+	price := OrderPriceCached(order)
+	amount := users[userName]
+
+	if amount >= price {
+		users[userName] -= price
+		return nil
+	}
+
+	return errors.New("insufficient funds")
+}
+
+// enum of UserNames possible sort orders
+const (
+	SORT_USERS_BY_NAME = iota
+	SORT_USERS_BY_NAME_REVERSED
+	SORT_USERS_BY_AMOUNT
+)
+
+// UserNames return slice of userNames in some order
+func UserNames(sortType int) []string {
+
+	var keys []string
+
+	switch sortType {
+	default:
+		fallthrough
+	case SORT_USERS_BY_NAME:
+		keys = keysSortedByName(users, false)
+	case SORT_USERS_BY_NAME_REVERSED:
+		keys = keysSortedByName(users, true)
+	case SORT_USERS_BY_AMOUNT:
+		keys = keysSortedByValue(users)
+	}
+
+	return keys
+}
+
+// keysSortedByName return keys of the map sorted in lexicographical order
+func keysSortedByName(myMap map[string]int, reversed bool) []string {
+	var keys []string
+	for k := range myMap {
+		keys = append(keys, k)
+	}
+
+	var toSort sort.Interface = sort.StringSlice(keys)
+	if reversed {
+		toSort = sort.Reverse(toSort)
+	}
+
+	sort.Sort(toSort)
+
+	return keys
+}
+
+// keysSortedByValue returns slice of keys sorted by the map's value
+func keysSortedByValue(myMap map[string]int) []string {
+	type KeyValue struct {
+		key   string
+		value int
+	}
+
+	var kvs []KeyValue
+	for k, v := range myMap {
+		kvs = append(kvs, KeyValue{k, v})
+	}
+
+	sort.Slice(kvs, func(i, j int) bool {
+		return kvs[i].value > kvs[j].value
+	})
+
+	var keys []string
+	for _, kv := range kvs {
+		keys = append(keys, kv.key)
+	}
+
+	return keys
+}
---
+++ map/map_shop_test.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestAddProduct(t *testing.T) {
+	// ignore adding errors handling
+	_ = AddProduct("Apple", 10)
+	_ = AddProduct("Milk", 10)
+	_ = AddProduct("Butter", 20)
+	_ = AddProduct("Bread", 30)
+
+	result := prices
+	test := map[string]int{"Apple": 10, "Bread": 30, "Butter": 20, "Milk": 10}
+
+	if !reflect.DeepEqual(result, test) {
+		t.Fatalf("Products weren't added to the map\n "+
+			"(%v vs %v).", result, test)
+	}
+}
+
+func TestUpdatePrice(t *testing.T) {
+	oldPrice := 10
+	newPrice := 20
+
+	_ = AddProduct("Apple", oldPrice)
+	_ = UpdatePrice("Apple", newPrice)
+
+	result := prices["Apple"]
+
+	if result != newPrice {
+		t.Fatalf("Can't updatem price of the product\n "+
+			"(%v vs %v).", result, newPrice)
+	}
+}
+
+func TestOrderPrice(t *testing.T) {
+	_ = AddProduct("Apple", 10)
+	_ = AddProduct("Milk", 10)
+	_ = AddProduct("Butter", 20)
+	_ = AddProduct("Bread", 30)
+
+	test := 400
+	result := OrderPrice(map[string]int{
+		"Apple":  3,
+		"Milk":   5,
+		"Butter": 4,
+		"Bread":  8})
+
+	if result != test {
+		if result != test {
+			t.Fatalf("Incorrect order price\n "+
+				"(%v vs %v).", result, test)
+		}
+	}
+}
+
+func BenchmarkOrderPrice(b *testing.B) {
+	_ = AddProduct("Apple", 10)
+	_ = AddProduct("Milk", 10)
+
+	order := map[string]int{"Apple": 10, "Milk": 20}
+
+	for i := 0; i < 1_000_000; i++ {
+		_ = OrderPrice(order)
+	}
+}
+
+func BenchmarkOrderPriceCached(b *testing.B) {
+	_ = AddProduct("Apple", 10)
+	_ = AddProduct("Milk", 10)
+
+	order := map[string]int{"Apple": 10, "Milk": 20}
+
+	for i := 0; i < 1_000_000; i++ {
+		_ = OrderPriceCached(order)
+	}
+}
+
+func TestAddUser(t *testing.T) {
+	_ = AddUser("Vasya", 100)
+	_ = AddUser("Vova", 1_000_000)
+
+	result := users
+	test := map[string]int{"Vasya": 100, "Vova": 1_000_000}
+
+	if !reflect.DeepEqual(result, test) {
+		t.Fatalf("Users weren't added to the map\n "+
+			"(%v vs %v).", result, test)
+	}
+}
+
+func TestMakeOrder(t *testing.T) {
+	_ = AddProduct("Apple", 10)
+	_ = AddProduct("Milk", 10)
+
+	_ = AddUser("Vasya", 100)
+	_ = AddUser("Vova", 1_000_000)
+
+	users := users
+	order := map[string]int{"Apple": 5, "Milk": 4}
+	_ = MakeOrder("Vasya", order)
+
+	if users["Vasya"] != 10 {
+		t.Fatalf("Wrong user amount value\n "+
+			"(%v vs %v).", users["Vasya"], 10)
+	}
+
+	ok := MakeOrder("Vasya", order)
+
+	if ok == nil {
+		t.Fatalf("The amount of money in the account "+
+			"went into a negative value.\n "+
+			"(%v).", users["Vasya"])
+	}
+
+	// t.Logf("Total amount '%v': %v", "Vasya", users["Vasya"])
+}
+
+func TestUserNames(t *testing.T) {
+	_ = AddUser("Vova", 1_000_000)
+	_ = AddUser("Katya", 1_999)
+	_ = AddUser("Petya", 250)
+	_ = AddUser("Vasya", 100)
+
+	printUsers := func(keys []string) {
+		for _, k := range keys {
+			fmt.Println(k, " ", users[k])
+		}
+		fmt.Println()
+	}
+
+	names := UserNames(SORT_USERS_BY_NAME)
+	printUsers(names)
+	names = UserNames(SORT_USERS_BY_NAME_REVERSED)
+	printUsers(names)
+	names = UserNames(SORT_USERS_BY_AMOUNT)
+	printUsers(names)
+}
