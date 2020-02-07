418d6806c12df50faebb43d166225053842d6def
hedgfog <hedg.fog@gmail.com>
2020-02-07 17:49:40 +0400

    Failed Tests#2

--- struct/hg_shop_test.go
+++ struct/hg_shop_test.go
@@ -6,18 +6,101 @@ import (
 )
 
 var (
-	ErrorPriceNotNegative = errors.New("total price not be negative/zero")
+	ErrorPriceNotNegative     = errors.New("total price not be negative/zero")
+	ErrorIncorrectAccountType = errors.New("incorrect account type")
 )
+
+func TestCalculateOrderSuccess(t *testing.T) {
+	type calcTest struct {
+		testName string
+		order    Order
+		total    float32
+	}
+
+	tests := []calcTest{
+		{
+			testName: "ProductsShouldBeInitialize",
+			order: Order{
+				Products: nil,
+				Bundles: []Bundle{
+					{
+						Products: []Product{
+							{
+								Name:  "Main",
+								Price: 100,
+								Type:  ProductNormal,
+							},
+							{
+								Name:  "Additional",
+								Price: 100,
+								Type:  ProductPremium,
+							},
+						},
+						Type:     BundleNormal,
+						Discount: 50,
+					},
+				},
+			},
+			total: 125,
+		},
+		{
+			testName: "ProductsShouldBeInitialize",
+			order: Order{
+				Products: []Product{},
+				Bundles: []Bundle{
+					{
+						Products: []Product{
+							{
+								Name:  "Main",
+								Price: 100,
+								Type:  ProductNormal,
+							},
+							{
+								Name:  "Additional",
+								Price: 100,
+								Type:  ProductPremium,
+							},
+						},
+						Type:     BundleNormal,
+						Discount: 50,
+					},
+				},
+			},
+			total: 125,
+		},
+	}
+	testShop := NewMarket()
+
+	testShop.Accounts["Bred"] = Account{
+		Name:    "Bred",
+		Balance: 100_000,
+		Type:    AccountNormal,
+	}
+
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			total, err := testShop.CalculateOrder("Bred", test.order)
+			if err != nil {
+				t.Fatalf("Test it should be success, error: %v", err)
+			}
+			if total != test.total {
+				t.Errorf("Error, type not correct: %v != %v", total, test.total)
+			}
+		})
+	}
+}
 
 func TestPlaceOrderFailed(t *testing.T) {
 	type productTest struct {
 		testName string
+		username string
 		order    Order
 		err      error
 	}
 
 	tests := []productTest{
 		{testName: "PriceNotBeZero",
+			username: "Bred",
 			order: Order{
 				Products: []Product{
 					{Name: "Pineapple", Price: 0, Type: ProductNormal},
@@ -29,6 +112,7 @@ func TestPlaceOrderFailed(t *testing.T) {
 			err: ErrorNegativeProductPrice,
 		},
 		{testName: "PriceNotBeNegative",
+			username: "Bred",
 			order: Order{
 				Products: []Product{
 					{Name: "Pineapple", Price: -100, Type: ProductNormal},
@@ -39,6 +123,16 @@ func TestPlaceOrderFailed(t *testing.T) {
 			},
 			err: ErrorNegativeProductPrice,
 		},
+		{testName: "BuyWithoutAddForPremium",
+			username: "Alfred",
+			order: Order{
+				Products: []Product{
+					{Name: "Pineapple", Price: 100, Type: ProductPremium},
+				},
+				Bundles: nil,
+			},
+			err: ErrorIncorrectAccountType,
+		},
 	}
 
 	testShop := NewMarket()
@@ -48,12 +142,17 @@ func TestPlaceOrderFailed(t *testing.T) {
 		Balance: 100_000,
 		Type:    AccountNormal,
 	}
+	testShop.Accounts["Alfred"] = Account{
+		Name:    "Alfred",
+		Balance: 1000,
+		Type:    100,
+	}
 
 	for _, test := range tests {
 		t.Run(test.testName, func(t *testing.T) {
-			err := testShop.PlaceOrder("Bred", test.order)
+			err := testShop.PlaceOrder(test.username, test.order)
 			if err == nil {
-				t.Errorf("Test it should be failed, error: %v, balance = %v", test.err, testShop.Accounts["Bred"].Balance)
+				t.Errorf("Test it should be failed, error: %v, balance = %v", test.err, testShop.Accounts[test.username].Balance)
 			} else if err.Error() != test.err.Error() && !errors.Is(err, test.err) {
 				t.Errorf("Values not equal: %v != %v", err, test.err)
 			}
